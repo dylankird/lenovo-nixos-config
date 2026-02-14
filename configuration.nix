78cc41b7d141370d796499a70bdb3ea6eab34cce
Dylan Kirdahy <me@dylankirdahy.nyc>
2026-02-13 19:46:53 -0500

    switch away from latest kernel, as it was breaking things

--- configuration.nix
+++ configuration.nix
@@ -40,8 +40,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Use latest kernel - disabled because this could break things on a stable release
+  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "lenovo-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
